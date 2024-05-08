c31235a681b43416de938c6dd7a8acabc3b149d7
Jayaprakashs-AdapptLimited <jayaprakash.s@adappt.ai>
2024-05-08 18:13:15 +0530

    Code refactor

--- android/app/src/Screens/ManageFriends.js
+++ android/app/src/Screens/ManageFriends.js
@@ -77,7 +77,6 @@ const Item = ({
   return selectedButton === 1 && acceptance ? (
     <View style={styles.item}>
       {/* List of Friends */}
-
       <View style={styles.othersLeaderboard}>
         <Image
           style={styles.othersProfileIcon}
@@ -87,10 +86,10 @@ const Item = ({
           alt="aa1 Logo"
         />
         <View style={styles.nameScore}>
-          <View style={{width: '70%'}}>
+          <View style={styles.nameContainer}>
             <Text style={styles.othersLeaderboardName}> {name} </Text>
           </View>
-          <View style={{width: '20%'}}>
+          <View style={styles.closeIconContainer}>
             <TouchableOpacity
               onPress={() => {
                 rejectRequest(acceptance, id);
@@ -102,9 +101,8 @@ const Item = ({
       </View>
     </View>
   ) : selectedButton === 2 && !acceptance ? (
-    <View style={styles.item}>
+    <View>
       {/* List of Friends */}
-
       <View style={styles.othersLeaderboard}>
         <Image
           style={styles.othersProfileIcon}
@@ -117,15 +115,9 @@ const Item = ({
           <View>
             <Text style={styles.othersLeaderboardName}> {name} </Text>
           </View>
-          <View
-            style={{
-              flexDirection: 'row',
-              flex: 1,
-              justifyContent: 'flex-end',
-              marginRight: 70,
-            }}>
+          <View style={styles.buttonContainer}>
             <TouchableOpacity
-              style={[styles.AcceptButton]}
+              style={[styles.AcceptRejectButton]}
               onPress={() => {
                 acceptRequest(id);
               }}>
@@ -133,7 +125,7 @@ const Item = ({
             </TouchableOpacity>
 
             <TouchableOpacity
-              style={[styles.AcceptButton, styles.notifyButton]}
+              style={[styles.AcceptRejectButton, styles.notifyButton]}
               onPress={() => rejectRequest(acceptance, id)}>
               <Text style={styles.acceptRejectText}>Reject</Text>
             </TouchableOpacity>
@@ -148,7 +140,7 @@ function ManageFriends() {
   const [modalVisible, setModalVisible] = useState(false);
   const [userName, setUserName] = useState({firstName: '', pin: ''});
   const [selectedButton, setSelectedButton] = useState(1);
-  const [friendsData, setFriendsData] = useState(DATA);
+  const [friendsData, setFriendsData] = useState([]);
 
   const onChangeName = (name, value) => {
     setUserName(previous => ({...previous, [name]: value}));
@@ -166,7 +158,7 @@ function ManageFriends() {
     setModalVisible(!modalVisible);
   };
 
-  // Function to accept request and update state
+  // To accept request and update state
   const acceptRequest = id => {
     const updatedData = friendsData.map(item => {
       if (item.id === id) {
@@ -177,8 +169,8 @@ function ManageFriends() {
     setFriendsData(updatedData);
   };
 
+  // To reject request and update state
   const rejectRequest = (acceptance, id) => {
-    console.log(id, 'iddd');
     if (!acceptance) {
       const updatedData = friendsData.filter(item => item.id !== id);
       setFriendsData(updatedData);
@@ -200,21 +192,10 @@ function ManageFriends() {
         <Header />
 
         {/*Friends Button Container */}
-        <View
-          style={{
-            flexDirection: 'row',
-            marginHorizontal: 16,
-            marginTop: 60,
-            borderWidth: 2,
-            borderColor: '#ff7c2b',
-          }}>
+        <View style={styles.groupInvitationBtnContainer}>
           <View style={styles.groupInvitationsButton}>
             <TouchableOpacity
-              style={[
-                styles.button,
-                styles.friendsButton,
-                selectedButton === 1 && styles.selected,
-              ]}
+              style={[styles.button, selectedButton === 1 && styles.selected]}
               onPress={() => setSelectedButton(1)}>
               <Text style={styles.text}>Your Group</Text>
             </TouchableOpacity>
@@ -234,20 +215,14 @@ function ManageFriends() {
         </View>
 
         {/* PinCode Button Container */}
-        <View
-          style={{
-            flexDirection: 'row',
-            marginHorizontal: 16,
-            marginTop: 18,
-          }}>
+        <View style={styles.addPinBtnsContainer}>
           <View style={styles.addFriendContainer}>
             <TouchableOpacity
-              style={[styles.inviteCodeButton]}
+              style={[styles.addFriendCodeButtons]}
               onPress={() => {
                 setModalVisible(!modalVisible);
-                // navigation.navigate('friendRequest');
               }}>
-              <View style={{flexDirection: 'row', alignItems: 'center'}}>
+              <View style={styles.iconText}>
                 <AntIcon name="pluscircleo" size={26} color="#FFF" />
 
                 <Text style={styles.text}>Add Friend</Text>
@@ -257,33 +232,17 @@ function ManageFriends() {
 
           <View style={styles.addFriendContainer}>
             <TouchableOpacity
-              style={[styles.inviteCodeButton]}
+              style={[styles.addFriendCodeButtons]}
               onPress={() => {
                 // setModalVisible(!modalVisible);
               }}>
-              <View style={{flexDirection: 'row', alignItems: 'center'}}>
+              <View style={styles.iconText}>
                 <AntIcon name="arrowright" size={26} color="#FFF" />
                 <Text style={styles.text}>Pincode</Text>
               </View>
             </TouchableOpacity>
           </View>
         </View>
-
-        {/* <FlatList
-          data={friendsData}
-          renderItem={({item}) => (
-            <Item
-              name={item.name}
-              image={item.image}
-              acceptance={item.acceptance}
-              id={item.id}
-              selectedButton={selectedButton}
-              acceptRequest={acceptRequest}
-              rejectRequest={rejectRequest}
-            />
-          )}
-          keyExtractor={item => item.id}
-        /> */}
 
         <FlatList
           data={
@@ -368,14 +327,14 @@ function ManageFriends() {
                 onPress={() => {
                   submitHandler();
                 }}
-                style={styles.searchButton}
+                style={styles.submitButton}
                 underlayColor="#fff">
                 <Text style={styles.searchText}>Submit</Text>
               </TouchableOpacity>
             </View>
             <View style={{marginTop: 15}}>
               <TouchableOpacity
-                style={styles.editNumberButton}
+                style={styles.backButton}
                 underlayColor="#fff"
                 onPress={() => {
                   // navigation.navigate('Register');
@@ -385,11 +344,6 @@ function ManageFriends() {
             </View>
           </View>
         </Modal>
-        {/* <TouchableOpacity
-          style={[styles.button, styles.buttonOpen]}
-          onPress={() => setModalVisible(true)}>
-          <Text style={styles.textStyle}>Show Modal</Text>
-        </TouchableOpacity> */}
       </View>
     </View>
   );
@@ -397,14 +351,12 @@ function ManageFriends() {
 
 export function Header() {
   return (
-    <View>
-      <View style={styles.logoAlign}>
-        <Image
-          style={styles.navLogo}
-          source={require('../assets/image/logo.png')}
-          alt="dr Logo"
-        />
-      </View>
+    <View style={styles.logoAlign}>
+      <Image
+        style={styles.navLogo}
+        source={require('../assets/image/logo.png')}
+        alt="dr Logo"
+      />
     </View>
   );
 }
@@ -424,141 +376,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     top: 40,
   },
-  screenTitle: {
-    textAlign: 'center',
-    color: '#e0d168',
-    fontSize: 22,
-    marginTop: 60,
-    fontWeight: 'bold',
-  },
-  subContent: {
-    color: '#fdeafe',
-    textAlign: 'center',
-    fontSize: 16,
-    marginTop: 10,
-    fontWeight: 'bold',
-  },
-  winnerButton: {
-    paddingTop: 10,
-    paddingRight: 18,
-    paddingBottom: 12,
-    paddingLeft: 18,
-    borderWidth: 1,
-    borderColor: '#8e7b78',
-    borderRadius: 6,
-  },
-  winnerText: {
-    color: '#ddd083',
-    textAlign: 'center',
-    fontSize: 16,
-    fontFamily: 'Inter-SemiBold',
-    fontWeight: 'bold',
-    textTransform: 'uppercase',
-  },
-  RevealBarContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    marginTop: 15,
-  },
-  backgroundImageContainer: {
-    position: 'absolute',
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0,
-    opacity: 0.2,
-  },
-  imageStyle: {
-    flex: 1,
-    height: 220,
-  },
-  container: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    paddingHorizontal: 20,
-    position: 'absolute',
-    marginTop: 100,
-  },
-  shortVerticalLine: {
-    width: 2,
-    height: 10,
-    backgroundColor: '#FFF',
-  },
-  horizontalLine: {
-    height: 3,
-    backgroundColor: '#FFF',
-    flex: 1,
-  },
-  dot: {
-    width: 10,
-    height: 10,
-    borderRadius: 5,
-    backgroundColor: '#FFF',
-  },
-  norwayDot: {
-    backgroundColor: '#b806f8',
-    position: 'absolute',
-    bottom: 0,
-  },
-  participantDot: {
-    backgroundColor: '#ff7c2b',
-    position: 'absolute',
-    bottom: 0,
-  },
-  dotVerticalLine: {
-    width: 3,
-    height: 50,
-    backgroundColor: '#FFF',
-  },
-  norwayDotVerticalLine: {
-    width: 3,
-    height: 100,
-    backgroundColor: '#b806f8',
-    marginTop: -104,
-    position: 'absolute',
-  },
-  participantDotVerticalLine: {
-    width: 3,
-    height: 140,
-    backgroundColor: '#ff7c2b',
-    marginTop: -124,
-    position: 'absolute',
-    bottom: 0,
-  },
-  valueText: {
-    position: 'absolute',
-    color: '#FFF',
-    bottom: 20,
-    marginHorizontal: 16,
-  },
-  scorePoster: {
-    bottom: 80,
-    color: '#000',
-    borderWidth: 0,
-    borderRadius: 8,
-    padding: 5,
-    backgroundColor: '#FFF',
-    fontWeight: 'bold',
-    fontSize: 16,
-    position: 'absolute',
-    // right: -40,
-  },
-  norwayPosterAlign: {
-    bottom: 104,
-    backgroundColor: '#b806f8',
-    color: '#FFF',
-  },
-  participantPosterAlign: {
-    backgroundColor: '#ff7c2b',
-    bottom: 140,
-    color: '#FFF',
-  },
-  maxValueText: {
-    position: 'absolute',
-    color: '#FFF',
-    bottom: 20,
-    right: -10,
-  },
   groupInvitationsButton: {
     flex: 1,
     // margin: 5,
@@ -570,26 +387,19 @@ const styles = StyleSheet.create({
   button: {
     padding: 10,
     elevation: 2,
-    // justifyContent: 'center',
     alignItems: 'center',
-    // backgroundColor: '#8900f8',
-    // marginHorizontal: 20,
   },
-  AcceptButton: {
+  AcceptRejectButton: {
     padding: 8,
     alignItems: 'center',
     backgroundColor: '#8900f8',
     marginHorizontal: 5,
   },
-  inviteCodeButton: {
+  addFriendCodeButtons: {
     padding: 10,
     elevation: 2,
-    // justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#8900f8',
-  },
-  friendsButton: {
-    // backgroundColor: '#FE8E2A',
   },
   invitationButton: {
     marginLeft: 3,
@@ -605,23 +415,10 @@ const styles = StyleSheet.create({
   othersLeaderboard: {
     flexDirection: 'row',
     alignItems: 'center',
-    // borderWidth: 1,
-    // borderColor: 'none',
     marginHorizontal: 16,
     padding: 12,
     backgroundColor: '#1f1061',
     marginTop: 18,
-  },
-  positionNumber: {
-    color: '#9684a2',
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  positionImage: {
-    // width: '20%',
-    // flexDirection: 'row',
-    // justifyContent: 'space-evenly',
-    // alignItems: 'center',
   },
   othersProfileIcon: {
     width: 50,
@@ -639,20 +436,12 @@ const styles = StyleSheet.create({
   },
   othersLeaderboardName: {
     color: '#ef8439',
-    // fontFamily: 'Inter-Bold',
     fontWeight: 'bold',
     fontSize: 16,
     marginLeft: 12,
   },
-  othersLeaderboardPercentage: {
-    color: '#FEA501',
-    fontWeight: 'bold',
-    fontSize: 18,
-    textAlign: 'right',
-  },
   logoViewAlign: {
     alignItems: 'center',
-    // marginTop: 50,
   },
   logo: {
     width: 250,
@@ -669,25 +458,9 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#FFF',
     fontSize: 16,
-    // marginTop: 20,
     fontFamily: 'Inter-Bold',
     marginLeft: 28,
     marginRight: 30,
-  },
-  loginScreenButton: {
-    marginRight: 40,
-    marginLeft: 40,
-    paddingTop: 10,
-    paddingRight: 24,
-    paddingBottom: 12,
-    paddingLeft: 24,
-    backgroundColor: '#FFF',
-  },
-  loginText: {
-    color: '#d5d5d7',
-    textAlign: 'center',
-    fontSize: 18,
-    fontWeight: 'bold',
   },
   textStyle: {
     color: '#FFF',
@@ -698,7 +471,7 @@ const styles = StyleSheet.create({
     marginLeft: 28,
     marginRight: 30,
   },
-  searchButton: {
+  submitButton: {
     marginRight: 40,
     marginLeft: 40,
     paddingTop: 10,
@@ -715,10 +488,9 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     textTransform: 'uppercase',
   },
-  editNumberButton: {
+  backButton: {
     marginRight: 40,
     marginLeft: 40,
-    // marginTop: 28,
     paddingTop: 10,
     paddingRight: 24,
     paddingBottom: 12,
@@ -740,15 +512,11 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'rgba(0, 0, 0, 0.7)',
     justifyContent: 'center',
-    // alignItems: 'center',
-    // marginTop: 22,
   },
   input: {
     height: 50,
-    // margin: 18,
     marginHorizontal: 38,
     borderWidth: 1,
-    // padding: 12,
     backgroundColor: '#FFF',
   },
   acceptRejectText: {
@@ -759,6 +527,34 @@ const styles = StyleSheet.create({
   },
   selected: {
     backgroundColor: '#ff9038',
+  },
+  nameContainer: {
+    width: '70%',
+  },
+  closeIconContainer: {
+    width: '20%',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    flex: 1,
+    justifyContent: 'flex-end',
+    marginRight: 70,
+  },
+  groupInvitationBtnContainer: {
+    flexDirection: 'row',
+    marginHorizontal: 16,
+    marginTop: 60,
+    borderWidth: 2,
+    borderColor: '#ff7c2b',
+  },
+  addPinBtnsContainer: {
+    flexDirection: 'row',
+    marginHorizontal: 16,
+    marginTop: 18,
+  },
+  iconText: {
+    flexDirection: 'row',
+    alignItems: 'center',
   },
 });
 
